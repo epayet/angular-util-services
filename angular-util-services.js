e7e172ddbbf812f06d130074cdad31424bc140e9
emmanuel.payet <jakspinning@gmail.com>
2014-06-05 11:30:56 +0200

    Add indexUtilController

--- angular-util-services.js
+++ angular-util-services.js
@@ -147,3 +147,10 @@ angular.module("angular-util-services").factory('WaitingService', ['$modal',
 
         return WaitingService;
     }]);
+
+angular.module(APP_NAME_CONTROLLERS).controller('IndexUtilController', [ '$scope', '$location',
+    function($scope, $location) {
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        }
+    }]);
